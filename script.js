7b67c4048bda33431d12c517023babdd1c7d840e
Sainath <71361447+sainath161@users.noreply.github.com>
2024-03-21 21:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function nextPalindrome(num) {
-  //your JS code here.
+ function isPalindrome(n) {
+        return n.toString() === n.toString().split('').reverse().join('');
+    }
+
+    // Helper function to find the next palindrome
+    function findNextPalindrome(n) {
+        while (true) {
+            n++;
+            if (isPalindrome(n)) {
+                return n;
+            }
+        }
+    }
+
+    // Convert input to a number
+    num = parseInt(num);
+
+    // Find the next palindrome
+    const nextPal = findNextPalindrome(num);
+
+    return nextPal;
 }
 
 const input = prompt("Enter a palindrome number");
